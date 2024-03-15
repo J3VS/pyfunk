717c63368143a57cbb41f64ac0c48df71e269b7f
jonathan <jviccary@emerlyn.com>
2024-03-15 12:56:21 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="pyfunk",
+    version="1.0.0",
+    description="python functional tools",
+    license="MIT",
+    author="jonathanviccary",
+    url="https://github.com/J3VS/pyfunk",
+    packages=find_packages(),
+    keywords="pyfunk",
+    install_requires=[
+    ],
+    zip_safe=True,
+)
